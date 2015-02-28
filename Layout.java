be70c9d397b420d35e94d4274702032decdbe3ee
jm588 <jm588@users.noreply.github.com>
2015-02-28 19:23:12 +0000

    Layout yo

---
+++ Layout.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by Katya on 28-Feb-15.
+ */
+public class Layout extends JFrame {
+    static Layout layout;
+    public Layout() {
+        super("KlikiT");
+
+    }
+}
